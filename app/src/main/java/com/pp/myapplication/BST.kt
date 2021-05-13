bbadef3a9992aa393b1e6c3bea900499d631aca8
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2021-05-13 12:19:47 +0100

    First commit

---
+++ app/src/main/java/com/pp/myapplication/BST.kt
@@ -0,0 +1,100 @@
+package com.pp.myapplication
+
+class BST<T : Comparable<T>> {
+    private var bstRoot: Node<T>? = null
+
+    fun add(node: T) {
+        add(node, bstRoot)
+    }
+
+    private fun add(t: T, root: Node<T>?) {
+        when {
+            root == null -> bstRoot = Node(t)
+            root.data == t -> Unit
+            root.data > t && root.left != null -> add(t, root.left)
+            root.data > t -> root.left = Node(t)
+            root.data < t && root.right != null -> add(t, root.right)
+            root.data < t -> root.right = Node(t)
+        }
+    }
+
+    fun contains(t: T): Boolean =
+        bstRoot?.let {
+            contains(it, t)
+        } ?: false
+
+    private fun contains(root: Node<T>, t: T): Boolean =
+        when {
+            root.data == t -> true
+            root.data > t && root.left != null -> contains(root.left!!, t)
+            root.data > t -> false
+            root.data < t && root.right != null -> contains(root.right!!, t)
+            root.data < t -> false
+            else -> false
+        }
+
+    fun remove(t: T) {
+        if (bstRoot == null || bstRoot!!.data == t) {
+            return
+        }
+        if (bstRoot!!.data > t && bstRoot!!.left != null) {
+            scan(t, bstRoot!!.left!!, bstRoot!!)
+        }
+        if (bstRoot!!.data < t && bstRoot!!.right != null) {
+            scan(t, bstRoot!!.right!!, bstRoot!!)
+        }
+    }
+
+    private fun scan(value: T, currentNode: Node<T>, parentNode: Node<T>) {
+        if (currentNode.data == value) {
+            remove(currentNode, parentNode)
+        }
+        if (currentNode.data > value && parentNode.left != null) {
+            scan(value, currentNode.left!!, currentNode)
+        }
+        if (currentNode.data < value && parentNode.right != null) {
+            scan(value, currentNode.right!!, currentNode)
+        }
+    }
+
+    private fun remove(node: Node<T>, parentNode: Node<T>) {
+        if (node.left == null && node.right == null) {
+            removeSingle(parentNode, node)
+        }
+        if ((node.left == null && node.right != null) || (node.right == null && node.left != null)) {
+            removeWithOneChild(parentNode, node)
+        }
+    }
+
+    private fun removeWithOneChild(parentNode: Node<T>, node: Node<T>) {
+        val child = if (node.left == null) {
+            node.right
+        } else {
+            node.left
+        }
+        if (parentNode.left != null && parentNode.left!!.data == node.data) {
+            parentNode.left = child
+        }
+        if (parentNode.right != null && parentNode.right!!.data == node.data) {
+            parentNode.right = child
+        }
+    }
+
+    private fun removeSingle(
+        parentNode: Node<T>,
+        node: Node<T>
+    ) {
+        if (parentNode.right == node) {
+            parentNode.right = null
+        }
+        if (parentNode.left == node) {
+            parentNode.left = null
+        }
+    }
+}
+
+data class Node<T : Comparable<T>>(
+    val data: T,
+    var left: Node<T>? = null,
+    var right: Node<T>? = null
+)
---
+++ app/src/test/java/com/pp/myapplication/Test.kt
@@ -0,0 +1,44 @@
+package com.pp.myapplication
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+
+class Test {
+
+    @Test
+    fun add_add_works() {
+        val bst = createBst()
+        assertThat(bst.contains(27)).isTrue()
+        assertThat(bst.contains(10)).isTrue()
+        assertThat(bst.contains(5)).isTrue()
+        assertThat(bst.contains(20)).isTrue()
+    }
+
+    @Test
+    fun remove_works() {
+        val bst = createBst()
+        bst.remove(27)
+        assertThat(bst.contains(27)).isFalse()
+    }
+
+    @Test
+    fun remove_oneworks() {
+        val bst = createBst()
+        bst.remove(15)
+        assertThat(bst.contains(15)).isFalse()
+    }
+
+    private fun createBst(): BST<Int> {
+        val bst = BST<Int>()
+        bst.add(10)
+        bst.add(5)
+        bst.add(20)
+        bst.add(1)
+        bst.add(7)
+        bst.add(15)
+        bst.add(27)
+        bst.add(19)
+        return bst
+    }
+}
